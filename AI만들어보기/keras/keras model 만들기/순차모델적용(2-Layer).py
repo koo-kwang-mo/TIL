57c6c7a8de69becba1aaf826c22a8e400fe57a8d
koo-kwang-mo <dpqpfptmxm3@naver.com>
2022-11-30 11:47:41 +0900

    layers 에러 수정

--- AI만들어보기/keras/keras model 만들기/순차모델적용(2-Layer).py
+++ AI만들어보기/keras/keras model 만들기/순차모델적용(2-Layer).py
@@ -16,10 +16,12 @@ model = keras.models.Sequential()
 #모델의 Layer 구성하기
 
 #첫번째 레이어로 1입력 , 2출력이다.
-model.add(keras.layer.Dense(2, input_shape(1,)))
+model.add(tf.keras.layers.Dense(2, input_shape=(1,)))
 
 #두번쨰 레이어는 입력 갯수를 추가해줄 필요가 없다. 1출력
-model.add(keras.layer.Dense(1))
+model.add(tf.keras.layers.Dense(1))
+#텐서플로우 2.xx버전 이상에서는 keras -> tf.keras로 바꾸면 에러해결
+
 
 #학습시키기
 
@@ -28,7 +30,7 @@ pre_train_y_predict = model.predict(x_test)
 print(pre_train_y_predict)
 
 model.compile('SGD', 'mse')
-hist = model.fit(x_train,y_train, epoch = 1000, batch_size = 2, verbose = 0)
+hist = model.fit(x_train,y_train, epochs = 1000, batch_size = 2, verbose = 0)
 
 y_predict = model.predict(x_test)
 print(y_predict)
