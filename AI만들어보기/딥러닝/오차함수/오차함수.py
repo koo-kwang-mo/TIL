e068deda1366b9659a38f97ba5fd5d46f1cab0d9
koo-kwang-mo <dpqpfptmxm3@naver.com>
2022-12-11 18:40:00 +0900

    오차함수 py 수정

--- AI만들어보기/딥러닝/오차함수/오차함수.py
+++ AI만들어보기/딥러닝/오차함수/오차함수.py
@@ -16,12 +16,14 @@ y = [i[1] for i in data]
 #예측
 predict_result = []
 
-def predict(x):
-    return line_a_b[0]*x + line_a_b[1]#기울기가 a, 절편이 b인 함수에 대입
-
+def predict2(t):
+    global line_a_b#predict2에서 사용하기 위해 선언
+    a = a = line_a_b[0]*t + line_a_b[1] #기울기가 a, 절편이 b인 함수에 대입
+    return a
+    
 for i in range(len(x)):
-    predict_result.append(predict(x[i]))
-    print("공부시간 = %.f, 실제점수 = %.f, 예측점수 = %.f"%(x[i], y[i], predict[x[i]]))
+    predict_result.append(predict2(x[i]))
+    print("공부시간 = %.f, 실제점수 = %.f, 예측점수 = %.f"%(x[i], y[i], predict2(x[i])))
 
 
 #MSE  구하기
@@ -31,6 +33,3 @@ def mse_val(predict_result, y):
 
 def mse(y_hat, y):#README에 있는 수식표현, 오차들 제곱의 평균
     return((y_hat-y)**2).mean()
-
-
-# 출처 : https://likethefirst.tistory.com/entry/Python-%EC%84%A0%ED%98%95%ED%9A%8C%EA%B7%80-2-a%EC%99%80-b%EC%97%90-%EB%94%B0%EB%A5%B8-%ED%8F%89%EA%B7%A0-%EC%98%A4%EC%B0%A8-%EC%A0%9C%EA%B3%B1MSE-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D-%ED%95%98%EA%B8%B0-%EC%84%A0%ED%98%95%ED%9A%8C%EA%B7%80-%EC%8B%A4%EC%8A%B5?category=935632
